d10d55453db2c7f7e869591e65ccf58d7b1f79fa
maxiaofei <2109015308@qq.com>
2016-12-26 20:02:39 +0800

    xiao

---
+++ app/src/main/java/myapp/bwei/com/maxiaofei/MainActivity.java
@@ -0,0 +1,17 @@
+package myapp.bwei.com.maxiaofei;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+    }
+}
